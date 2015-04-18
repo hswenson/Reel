17b900d433bbf93fd3d9361e5c7afbcac43f689c
Hannah Swenson <hswenson@princeton.edu>
2015-04-18 16:22:39 -0400

    parse

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
+var mongoose = require('mongoose');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -9,6 +10,14 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
+});
+
+Parse.initialize("dSMMZyBYFJCHyBsg6T4QZUj1Mta0enLbwE0iRj1R", "UF6lsnickYkZYdrizLB8vIBCfZsMqx4b0fd1BQGN");
+
+var TestObject = Parse.Object.extend("TestObject");
+var testObject = new TestObject();
+testObject.save({foo: "bar"}).then(function(object) {
+  alert("yay! it worked");
 });
 
 app.listen(app.get('port'), function() {
